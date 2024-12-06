14eac01b1aa161f45ae014cde1186dc436d6fd17
isacapp <isa.cappellino@gmail.com>
2024-12-06 11:18:27 +0100

    refactor: updated schema with hashed password

--- schema.sql
+++ schema.sql
@@ -22,8 +22,9 @@ CREATE TABLE users (
 
 
 INSERT INTO users (username, email, password_hash) VALUES
-  ('admin', 'admin@gmail.com', 'root'),
-  ('isa_cap', 'isa.cappellino@gmail.com', 'cappellino');
+  ('admin', 'admin@gmail.com', 'scrypt:32768:8:1$UsmfbeH7UMeMQNaq$302f5204408ccd107b53db898a6adb0b0d23a20278fd1c929f8ef4707bee4721052168b8ff26a68ddf1dcc9f19b26eb01509ceb98798d626a0c1ad79837c9523'),
+  ('isa_cap', 'isa.cappellino@gmail.com', 'scrypt:32768:8:1$dC69zmnF7O9P56E9$fa905c5125c931f3ace21cca11723e29cff668fb07cbd489822107f15c5a1fdee9025542c47e47c4fa485da8cf94727dbe3246fb1c9aa8a6c9ec2c60c67d16b1');
+
 
 INSERT INTO events (name, start_time, end_time, dates) VALUES
   ('Event 1', '10:00:00', '12:00:00', '2021-10-01, 2021-10-02, 2021-10-03'),
